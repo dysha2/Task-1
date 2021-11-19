d2b3f00ae383921cdffa4c9b27231ddcdfd22a8c
dyha035@gmail.com <dyha035@gmail.com>
2021-11-19 17:48:37 +0500

    Добавьте файлы проекта.

---
+++ Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(File.ReadAllText("input.txt"));
+            int dayOfStudent = day + 3 < 7 ? day + 3 : day + 3 - 7;
+            File.WriteAllText("output.txt", dayOfStudent.ToString());
+        }
+    }
+}
